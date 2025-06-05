8885d339939d3575f81ec066da6c87a1e9615cdf
dwiyyl <dwiyulianti327@gmail.com>
2025-06-05 15:48:06 +0700

    mengupdate class MenuView

--- src/util/MenuView.java
+++ src/util/MenuView.java
@@ -7,6 +7,7 @@ public class MenuView {
         System.out.println("2. Batalkan Reservasi Terakhir");
         System.out.println("3. Lihat Reservasi Terakhir");
         System.out.println("4. Tampilkan Semua Reservasi");
-        System.out.println("5. Keluar");
+        System.out.println("5. Update Reservasi"); 
+        System.out.println("6. Keluar");
     }
 }
